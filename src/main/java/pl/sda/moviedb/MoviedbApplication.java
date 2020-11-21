0082dd1be33547b84b695c19bc57e17776bb2570
Tomasz.Rutyna <Poniatowa2021!>
2020-11-21 13:13:50 +0100

    init commit

---
+++ src/main/java/pl/sda/moviedb/MoviedbApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.moviedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviedbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviedbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/moviedb/MoviedbApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.moviedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviedbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
